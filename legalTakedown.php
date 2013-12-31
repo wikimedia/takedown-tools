956f12fe9fdce396d46fc4a3fde6ef3fd770f4d8
jamesur <jalexander@wikimedia.org>
2013-12-31 02:23:37 -0800

    add lcatools links to side bar, logged in text and logout button

--- legalTakedown.php
+++ legalTakedown.php
@@ -8,6 +8,7 @@
 	<script src='scripts/moment.min.js'></script>
 	<script src='scripts/pikaday.js'></script>
 	<script src='scripts/pikaday.jquery.js'></script>
+	<script src='scripts/lca.js'></script>
 	<script>
 	$(document).ready(function(){
 
@@ -214,12 +215,12 @@
 				<div role="navigation" id="p-personal" class="portlet">
 					<div class="pBody">
 						<ul>
-							<li id="pt-login">You are logged in as <?php echo $_SERVER['AUTHENTICATE_USER']; ?> </li>
+							<li id="pt-login">You are logged in as <b><u><?php echo $_SERVER['PHP_AUTH_USER']; ?></u></b> if you are done or not you <b><a href="#" onclick="javascript: logout();">log out </a> </b></li>
 						</ul>
 					</div>
 				</div>
 				<div class='portlet' id='p-logo' role='banner'>
-					<a href='legalTakedown.html' title='Back to form'></a>
+					<a href='index.php' title='Back home'></a>
 				</div>
 				<div class='no-text-transform portlet' id='p-navigation' role='navigation'>
 					<h3>LCA links</h3>
@@ -233,6 +234,29 @@
 							</li>
 							<li id='collabwiki-link'>
 								<a href='https://collab.wikimedia.org/wiki/Main_Page'>Collab Wiki</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class='no-text-transform portlet' id='p-navigation' role='navigation'>
+					<h3>LCATools Forms</h3>
+					<div class='pBody'>
+						<ul>
+							<li id='dmca-takedown-form'>
+								<a href="legalTakedown.php">DMCA Takedown Form</a>
+							</li>
+							<li id='ncmec-form'>
+								<a href="NCMECreporting.php"> Child Protection Takedown Form </a>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class='no-text-transform portlet' id='p-navigation' role='navigation'>
+					<h3>LCATools Special Pages</h3>
+					<div class='pBody'>
+						<ul>
+							<li id='dmca-takedown-form'>
+								<a href="CentralLog.php"> Central submission Log </a>
 							</li>
 						</ul>
 					</div>
