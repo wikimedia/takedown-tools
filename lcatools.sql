578c9fac182c4ea155291f64aba69212313b2c33
jamesur <jalexander@wikimedia.org>
2014-01-02 01:17:39 -0800

    update db creation script

--- lcatools.sql
+++ lcatools.sql
@@ -1,36 +1,32 @@
--- MySQL dump 10.13  Distrib 5.5.24, for osx10.7 (i386)
---
--- Host: localhost    Database: lcatools
--- ------------------------------------------------------
--- Server version	5.5.24
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Current Database: `lcatools`
---
 
 CREATE DATABASE /*!32312 IF NOT EXISTS*/ `lcatools` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
 USE `lcatools`;
-
---
--- Table structure for table `centrallog`
---
-
-DROP TABLE IF EXISTS `centrallog`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `centrallog` (
+CREATE TABLE /*!32312 IF NOT EXISTS*/ `basicrelease` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `log_id` int(11) NOT NULL,
+  `user` varchar(255) NOT NULL,
+  `timestamp` datetime NOT NULL,
+  `who_received` text,
+  `pre_approved` varchar(3) DEFAULT NULL,
+  `why_released` text,
+  `who_released` varchar(255) DEFAULT NULL,
+  `who_released_to` varchar(255) DEFAULT NULL,
+  `released_to_contact` text,
+  `details` text,
+  `test` char(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `log_id` (`log_id`),
+  KEY `logid` (`log_id`),
+  KEY `user_submitted` (`user`),
+  KEY `test_submission` (`test`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE /*!32312 IF NOT EXISTS*/ `centrallog` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user` varchar(255) NOT NULL,
   `timestamp` datetime DEFAULT NULL,
@@ -41,16 +37,5 @@ CREATE TABLE `centrallog` (
   KEY `submit_user` (`user`),
   KEY `submit_type` (`type`),
   KEY `is_test` (`test`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2013-12-31  4:00:59
