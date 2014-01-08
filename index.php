743030938762a00da8dcefa900014c9f6478ea36
jamesur <jalexander@wikimedia.org>
2014-01-08 15:18:00 -0800

    break in index

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
 
 					<u> 'special' pages </u> <br />
 					<a href="centralLog.php"> Central submission Log </a> <br />
-					<a href="NCMECretract.php"> NCMEC report retraction form </a> (note: Only for unclosed test submissions which errored out)
+					<a href="NCMECretract.php"> NCMEC report retraction form </a> <br /> (note: Only for unclosed test submissions which errored out)
 			</div>
 
 			</div>
