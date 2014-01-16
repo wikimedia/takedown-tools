338cc8dc54946e049f708b1f43ab3784ed7315d6
jamesur <jalexander@wikimedia.org>
2014-01-16 09:14:35 -0800

    add test page for oauth to index

--- index.php
+++ index.php
@@ -54,6 +54,7 @@
 					<u> 'special' pages </u> <br />
 					<a href="centralLog.php"> Central submission Log </a> <br />
 					<a href="beginoauthregistration.php"> Register with Mediawiki OAuth <br /> (BETA)</a> <br />
+					<a href="testOAuth.php"> Test your stored OAuth data </a> <br />
 					<a href="NCMECretract.php"> NCMEC report retraction form </a> <br /> (note: Only for unclosed test submissions which errored out)</a>
 			</div>
 
