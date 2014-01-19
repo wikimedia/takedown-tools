8f73de4c7628c529f4a78a19a070ec5af0dc7454
jamesur <jalexander@wikimedia.org>
2014-01-18 20:53:56 -0800

    more flexible base

--- examples/NCMECproofofconcept.php
+++ examples/NCMECproofofconcept.php
@@ -17,7 +17,7 @@ And this is a 3rd...............a bit longer then the 2nd';
 <!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Strict//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd'>
 <html xmlns='http://www.w3.org/1999/xhtml' lang='en-US' xml:lang='en-US'>
 <head>
-    <base href='https://lcatools.corp.wikimedia.org'>
+    <base href='..'>
 	<link rel='shortcut icon' href='images/favicon.ico'/>
 	<title>NCMEC Submission</title>
 	<meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
