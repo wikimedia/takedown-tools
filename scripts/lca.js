3de1f0cbf1aa1b9aa640744433e6abe638e8bda3
jamesur <jalexander@wikimedia.org>
2014-01-25 21:13:20 -0800

    code tidy

--- scripts/lca.js
+++ scripts/lca.js
@@ -1,26 +1,26 @@
 
 //logout from system, taken from a couple different places but especially http://stackoverflow.com/questions/233507/how-to-log-out-user-from-web-site-using-basic-authentication 
 function logout(Loc){
-    var outcome, u, m = "You should be logged out now.";
+    var outcome, u, m = 'You should be logged out now.';
 
-    try { outcome = document.execCommand("ClearAuthenticationCache") }catch(e){}
+    try { outcome = document.execCommand('ClearAuthenticationCache'); }catch(e){}
 
     if (!outcome) {
 
         outcome = (function(x){
             if (x) {
-                x.open("HEAD", Loc || location.href, true, "logout", (new Date()).getTime().toString())
-                x.send("")
+                x.open('HEAD', Loc || location.href, true, 'logout', (new Date()).getTime().toString());
+                x.send('');
 
-                return 1 
+                return 1;
             } else {
-                return
+                return;
             }
-        })(window.XMLHttpRequest ? new window.XMLHttpRequest() : ( window.ActiveXObject ? new ActiveXObject("Microsoft.XMLHTTP") : u ))
+        })(window.XMLHttpRequest ? new window.XMLHttpRequest() : ( window.ActiveXObject ? new ActiveXObject('Microsoft.XMLHTTP') : u ));
     }
     if (!outcome) {
-        m = "Hmmm, looks like something went wrong, try closing your browser to fully log out"
+        m = 'Hmmm, looks like something went wrong, try closing your browser to fully log out';
     }
-    alert(m)
+    alert(m);
 
 }
