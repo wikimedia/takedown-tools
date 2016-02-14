71cd86ee6bc66f52e8ed2eb7078692f7f1d956c5
jamesur <jalexander@wikimedia.org>
2016-02-13 23:24:11 -0800

    update side bar for comments page where users do not have to have access to more private tools

--- 2016 Strategy/page-strategy.php
+++ 2016 Strategy/page-strategy.php
@@ -7,17 +7,25 @@
 					</div>
 				</div>
 				<div class='portlet' id='p-logo' role='banner'>
-					<a href='/' title='Back home'></a>
 				</div>
 				<div class='no-text-transform portlet' id='p-navigation' role='navigation'>
 					<h3>Strategy links</h3>
 					<div class='pBody'>
 						<ul>
 							<li id='strategy-mainpage-link'>
-								<a href='http://meta.wikimedia.org/wiki/2015_Strategy/Community_consultation'>2015 Strategy Consultation</a>
+								<a href='https://meta.wikimedia.org/wiki/2016_Strategy/Community_consultation'>2016 Strategy Consultation (meta)</a>
 							</li>
-							<li id='officewiki-link'>
-								<a href='http://meta.wikimedia.org/wiki/Talk:2015_Strategy/Community_consultation'>Strategy Consultation comments page</a>
+							<li id='strategy-reach-link'>
+								<a href='https://meta.wikimedia.org/wiki/Talk:2016_Strategy/Reach'>Reach comments page (Meta)</a>
+							</li>
+							<li id='strategy-communities-link'>
+								<a href='https://meta.wikimedia.org/wiki/Talk:2016_Strategy/Communities'>Communities comments page (Meta)</a>
+							</li>
+							<li id='strategy-knowledge-link'>
+								<a href='https://meta.wikimedia.org/wiki/Talk:2016_Strategy/Knowledge'>Knowledge comments page (Meta)</a>
+							</li>
+							<li id='strategy-officewiki-link'>
+								<a href='https://office.wikimedia.org/wiki/2016_Strategy'>OfficeWiki Strategy page</a>
 							</li>
 						</ul>
 					</div>
